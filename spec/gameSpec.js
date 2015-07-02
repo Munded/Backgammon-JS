ee7e673705716a5d554f51824a3f47dfb3f37394
Edmund O'Brien <edobrien@Edmunds-MacBook-Air.local>
2015-07-02 15:30:42 +0100

    added tests for white pieces

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,15 +1,39 @@
 describe('Game', function() {
+	beforeEach(function() {
+		game = new Game
+	})
 
 	describe('initialising the board', function() {
 
 		it('initialises with 24 spots on the board', function() {
-			game = new Game
 			expect(Object.keys(game.board).length).toEqual(24)
 		})
 
 		it('each spot is an array', function() {
-			game = new Game
 			expect(game.board[2]).toEqual([])
+		})
+
+		describe('placing white pieces', function() {
+
+			it('2 white pieces in (1)', function(){
+				expect(game.board[1].length).toEqual(2)
+				expect(game.board[1][0].colour).toEqual("white")
+			})
+
+			it('5 white pieces in (12)', function(){
+				expect(game.board[12].length).toEqual(5)
+				expect(game.board[12][0].colour).toEqual("white")
+			})
+
+			it('3 white pieces in (17)', function(){			
+				expect(game.board[17].length).toEqual(3)
+				expect(game.board[17][0].colour).toEqual("white")
+			})
+
+			it('5 white pieces in (19)', function(){	
+				expect(game.board[19].length).toEqual(5)
+				expect(game.board[19][0].colour).toEqual("white")
+			})
 		})
 
 	})
--- src/game.js
+++ src/game.js
@@ -1,10 +1,15 @@
 var Game = function() {
 	this.board = {}
 	this.boardCreation()
-}
+};
 
 Game.prototype.boardCreation = function() {
 	for(i=1; i <25; i++) {
 		this.board[i] = []
 	}
+	this.piecePlacement()
+};
+
+Game.prototype.piecePlacement = function() {
+	this.board[1].push(new Piece('white'))
 };
